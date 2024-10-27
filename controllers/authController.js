f2d7e14e26b610bd5c6230a5ec497de4633e2ca9
codeking2024 <lightfury224713@gmail.com>
2024-10-27 09:38:41 +0100

    fix: env file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,57 +1,57 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
-const logger = require('../utils/logger');
-const secret = process.env.SECRET || 'some other secret as default';
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const logger = require("../utils/logger");
+const secret = process.env.SECRET || "some other secret as default";
 
-require('dotenv').config();
+require("dotenv").config();
 
 exports.register = async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        // Check if the user already exists
-        const existingUser = await User.findOne({ username });
-        if (existingUser) return res.status(400).json({ message: 'User already exists' });
+  const { username, password } = req.body;
+  try {
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser)
+      return res.status(400).json({ message: "User already exists" });
 
-        const newUser = new User({ username, password });
-        await newUser.save();
-        
-        logger(`User registered: ${username}`);
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    logger(`User registered: ${username}`);
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 exports.login = async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const user = await User.findOne({ username }).select("+password");
-        if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({ username }).select("+password");
+    if (!user) return res.status(401).json({ message: "No such User" });
 
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
 
-        const payload = {
-            id: user._id,
-            username: user.username
-        };
-    
-        token = await jwt.sign(payload, secret, { expiresIn: 36000 });
+    const payload = {
+      id: user._id,
+      username: user.username,
+    };
 
-        if (!token) {
-            return res.status(500)
-                .json({ error: "Error signing token",
-                    raw: err });
-        }
+    token = await jwt.sign(payload, secret, { expiresIn: 3600 });
 
-        logger(`User logged in: ${username}`);
-
-        return res.json({
-        success: true,
-        token: `Bearer ${token}` });
-
-    } catch (error) {
-        res.status(500).json({ message: error.message });
+    if (!token) {
+      return res.status(500).json({ error: "Error signing token", raw: err });
     }
+
+    logger(`User logged in: ${username}`);
+
+    return res.json({
+      success: true,
+      token: `Bearer ${token}`,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
