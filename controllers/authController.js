fe64470274982cd7ff7796f4ad5835f80ec014d3
blueSky009-dev <web3dev009@gmail.com>
2024-10-29 00:33:24 +0800

    fix: cors error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
@@ -8,56 +8,68 @@ const secret = process.env.SECRET || "some other secret as default";
 require("dotenv").config();
 
 exports.register = async (req, res) => {
-
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
-  const { firstName, lastName, email, password, role, enrollmentNumber, firstYearOfStudy } = req.body;
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    role,
+    enrollmentNumber,
+    firstYearOfStudy,
+  } = req.body;
 
   if (role === 2 && (!enrollmentNumber || !firstYearOfStudy)) {
-    return res.status(400).json({ message: 'Enrollment number and first year of study are required for students.' });
+    return res
+      .status(400)
+      .json({
+        message:
+          "Enrollment number and first year of study are required for students.",
+      });
   }
 
-  console.log(typeof(role));
+  console.log(typeof role);
   if (![0, 1, 2].includes(Number(role))) {
-    return res.status(400).json({ message: 'Invalid role' });
+    return res.status(400).json({ message: "Invalid role" });
   }
 
   try {
-   
-      // Check if the user already exists
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-          return res.status(400).json({ message: 'User already exists' });
-      }
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
 
-      const newUser = new User({
-        firstName,
-        lastName,
-        email,
-        password,
-        role,
-        enrollmentNumber: role === 2 ? enrollmentNumber : undefined,
-        firstYearOfStudy: role === 2 ? firstYearOfStudy : undefined,
-        avatar: req.file ? `/uploads/images/avatar/${req.file.filename}` : undefined,
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password,
+      role,
+      enrollmentNumber: role === 2 ? enrollmentNumber : undefined,
+      firstYearOfStudy: role === 2 ? firstYearOfStudy : undefined,
+      avatar: req.file
+        ? `/uploads/images/avatar/${req.file.filename}`
+        : undefined,
     });
 
-      await newUser.save();
-      logger(`User registered: ${email}`);
-      res.status(201).json({ message: 'User registered successfully' });
+    await newUser.save();
+    logger(`User registered: ${email}`);
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-      console.error('Error registering user:', error);
-      res.status(500).json({ message: 'Server error' });
+    console.error("Error registering user:", error);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
 exports.login = async (req, res) => {
-
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+    return res.status(400).json({ errors: errors.array() });
   }
 
   const { email, password } = req.body;
