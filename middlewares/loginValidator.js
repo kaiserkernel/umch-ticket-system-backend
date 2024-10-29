8b196eb224aef94f2e01d7d0123711efda01530b
codeking2024 <lightfury224713@gmail.com>
2024-10-29 18:01:43 +0100

    fix

--- middlewares/loginValidator.js
+++ middlewares/loginValidator.js
@@ -1,19 +1,21 @@
-const { body } = require('express-validator');
+const { body } = require("express-validator");
 
 const loginValidator = [
-    body('email')
-        .if((value, { req }) => req.body.role !== "2")
-        .notEmpty().withMessage("Email is required for non-students")
-        .isEmail().withMessage("Invalid email format"),
+  body("email")
+    .if((value, { req }) => req.body.role !== "2")
+    .notEmpty()
+    .withMessage("Email is required for non-students")
+    .isEmail()
+    .withMessage("Invalid email format"),
 
-    body('enrollmentNumber')
-        .if((value, { req }) => req.body.role === "2")
-        .notEmpty().withMessage("Enrollment number is required for students")
-        .isString().withMessage("Enrollment number must be a string"),
+  body("enrollmentNumber")
+    .if((value, { req }) => req.body.role === "2")
+    .notEmpty()
+    .withMessage("Enrollment number is required for students")
+    .isString()
+    .withMessage("Enrollment number must be a string"),
 
-    body('password')
-        .notEmpty().withMessage("Password is required")
-        .isLength({ min: 6 }).withMessage("Invalid password"),
+  body("password").notEmpty().withMessage("Password is required"),
 ];
 
 module.exports = loginValidator;
